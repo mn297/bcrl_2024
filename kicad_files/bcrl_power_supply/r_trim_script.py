00f7595b45ae757f81825fcd4319d55ff89973ef
Your Name <peterwayne3293@gmail.com>
2024-03-11 01:19:03 -0400

    ...

--- kicad_files/bcrl_power_supply/r_trim_script.py
+++ kicad_files/bcrl_power_supply/r_trim_script.py
@@ -10,10 +10,31 @@ def compute_rtrim_pdq10_q48_s5(Vo_desired):
     # Calculate trim resistors
     rtrim_up = ((Vr * R1 * (R2 + R3)) / ((Vo_desired - Vo_nom) * R2)) - Rt
     rtrim_down = R1 * ((Vr * R1) / ((Vo_nom - Vo_desired) * R2) - 1) - Rt
-    print(f"For Vo = {Vo_desired}V:")
+    print(f"pdq10_q48_s5 For Vo = {Vo_desired}V:")
     print(f"    R_trim_up = {rtrim_up} Ω")
     print(f"    R_trim_down = {rtrim_down} Ω")
 
 
 compute_rtrim_pdq10_q48_s5(5.5)
 compute_rtrim_pdq10_q48_s5(4.5)
+
+
+def compute_rtrim_pqc50(Vnom, Vout):
+    delta_percent = abs(((Vnom - Vout) / Vnom) * 100)
+
+    rtrim_up = (
+        ((5.11 * Vnom * (100 + delta_percent)) / (1.225 * delta_percent))
+        - (511 / delta_percent)
+        - 10.22
+    )
+    rtrim_down = (511 / delta_percent) - 10.22
+    # print(f"delta_percent: {delta_percent}%")
+    print(f"pqc50 For Vo = {Vout}V:")
+    print(f"    Rtrim Up: {rtrim_up * 1000} Ω")
+    print(f"    Rtrim Down: {rtrim_down * 1000} Ω")
+
+
+# Replace these with your actual values
+Vnom = 5.0  # Nominal voltage
+Vout_up = 8  # Desired output voltage
+compute_rtrim_pqc50(Vnom, Vout_up)
